4e5ad0dac4ae641527944563d0d82df72fc91a75
denkatakostadinov <40743092+denkatakostadinov@users.noreply.github.com>
2019-02-08 18:26:40 +0200

    Add files via upload

---
+++ SlavePi-html/ipconf.php
@@ -0,0 +1,4 @@
+<?php
+   $outcome = shell_exec('/home/pi/Desktop/CHANGE_IP.sh');
+  // echo $outcome;
+?>
---
+++ SlavePi-html/template.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> <html>
+<a class="weatherwidget-io" href="https://forecast7.com/en/34d05n118d24/los-angeles/?unit=us" data-label_1="LOS ANGELES" data-label_2="WEATHER" data-font="Times New Roman" data-icons="Climacons Animated" data-days="5" data-theme="fall-leaves" >LOS ANGELES WEATHER</a>
+<script>
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+</script>
+
+ <head>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <META_REFRESH> <script language="JavaScript"> <!-- //preload the 
+next image... if (document.images) {
+    var pic1 = new Image();
+    pic1.src="<NEXT_IMAGE>";
+}
+//-->
+</script>
+
+ <style type="text/css"> 
+body {
+	background-image:url("/background/Orange-space.jpg");
+	text-align:center;
+	font-size:10px;
+	font-family:Verdana, Helvetica, sans-serif;
+	color:#999;
+	}
+.image_info {
+    display:block;
+    font-size:10px;
+    color:#999;
+    border:0px;
+    }
+.wrapper {
+    margin:0px auto;
+    padding-top:0px;
+    padding-left:0px;
+    padding-right:0px;
+    padding-bottom:0px;
+    width:700px;
+    border: 0px solid #000;
+    text-align:left;
+}
+.show_title a {
+    font-size:12px;
+    text-decoration:none;
+    color:#999;
+}
+.image_info a {
+    text-decoration:none;
+    color:#fff;
+}
+img {
+	border-width: 0;
+}
+.image {
+    margin-bottom:5px;
+}
+.image img {
+
+    max-width:1080px;
+    max-height:1000px;
+    width:;
+    height:auto;
+}
+
+.small, .small a {
+    margin-top: 20px;
+    text-decoration:none;
+    color:#777;
+}
+</style> </head> <body> <div class='wrapper'>
+      <div class='navigation'>
+       [<POSITION>]
+	   </div>
+    <div class='image'>
+        <br />
+        <IMAGE>
+        <br />
+        <IMAGE_TITLE>&nbsp;
+        <EXIF_COMMENT>
+    </div>
+
+</body>
+</html>
